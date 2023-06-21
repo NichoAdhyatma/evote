22677ef6eaacdd24677acaab8323fcb1c3948c02
Nicholaus Adhyatma <nicholausadhyatma@gmail.com>
2023-06-21 16:07:43 +0700

    profile-info

--- resources/js/Pages/Profile/Edit.jsx
+++ resources/js/Pages/Profile/Edit.jsx
@@ -1,6 +1,4 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import DeleteUserForm from "./Partials/DeleteUserForm";
-import UpdatePasswordForm from "./Partials/UpdatePasswordForm";
 import UpdateProfileInformationForm from "./Partials/UpdateProfileInformationForm";
 import { Head } from "@inertiajs/react";
 
@@ -24,10 +22,6 @@ export default function Edit({ auth, mustVerifyEmail, status }) {
                             status={status}
                             className="max-w-xl"
                         />
-                    </div>
-
-                    <div className="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                        <DeleteUserForm className="max-w-xl" />
                     </div>
                 </div>
             </div>
--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.jsx
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.jsx
@@ -1,9 +1,6 @@
 import InputError from "@/Components/InputError";
-import InputLabel from "@/Components/InputLabel";
-import PrimaryButton from "@/Components/PrimaryButton";
 import TextInput from "@/Components/TextInput";
 import { Link, useForm, usePage } from "@inertiajs/react";
-import { Transition } from "@headlessui/react";
 
 export default function UpdateProfileInformation({
     mustVerifyEmail,
@@ -12,40 +9,32 @@ export default function UpdateProfileInformation({
 }) {
     const user = usePage().props.auth.user;
 
-    const { data, setData, patch, errors, processing, recentlySuccessful } =
-        useForm({
-            name: user.name,
-            email: user.email,
-        });
-
-    const submit = (e) => {
-        e.preventDefault();
-
-        patch(route("profile.update"));
-    };
+    const { data, setData, errors } = useForm({
+        name: user.name,
+        email: user.email,
+    });
 
     return (
         <section className={className}>
             <header>
                 <h2 className="text-lg font-medium text-gray-900">
-                    Profile Information
+                    Informasi Profil
                 </h2>
 
                 <p className="mt-1 text-sm text-gray-600">
-                    Update your account's profile information and email address.
+                    Informasi nama dan email akun profil
                 </p>
             </header>
 
-            <form onSubmit={submit} className="mt-6 space-y-6">
+            <div className="mt-6 space-y-6">
                 <div>
                     <TextInput
-                        id="name"
                         label="Nama"
                         className="mt-1 block w-full"
                         value={data.name}
-                        onChange={(e) => setData("name", e.target.value)}
                         required
                         isFocused
+                        disabled
                         autoComplete="name"
                     />
 
@@ -54,12 +43,10 @@ export default function UpdateProfileInformation({
 
                 <div>
                     <TextInput
-                        id="email"
                         label="Email"
-                        type="email"
                         className="mt-1 block w-full"
                         value={data.email}
-                        onChange={(e) => setData("email", e.target.value)}
+                        disabled
                         required
                         autoComplete="username"
                     />
@@ -89,26 +76,7 @@ export default function UpdateProfileInformation({
                         )}
                     </div>
                 )}
-
-                <div className="flex items-center gap-4">
-                    <PrimaryButton
-                        disabled={processing}
-                        loading={processing}
-                        type="submit"
-                    >
-                        Save
-                    </PrimaryButton>
-
-                    <Transition
-                        show={recentlySuccessful}
-                        enterFrom="opacity-0"
-                        leaveTo="opacity-0"
-                        className="transition ease-in-out"
-                    >
-                        <p className="text-sm text-gray-600">Saved.</p>
-                    </Transition>
-                </div>
-            </form>
+            </div>
         </section>
     );
 }
