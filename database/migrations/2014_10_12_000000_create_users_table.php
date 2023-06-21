c7913af79ac118616b3a0d6c772e34c08f189277
AradheaRangga <aradhearangga36973@gmail.com>
2023-06-21 14:21:49 +0700

    fixing migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('token')->default(null);
+            $table->string('token')->nullable();
             $table->enum('level', ['admin', 'user'])->default('user');
-            $table->string('image')->default(null);
+            $table->string('image')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
