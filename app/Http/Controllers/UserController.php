4fd256a270f27f49f0cf66295818271a4127cc3b
AradheaRangga <aradhearangga36973@gmail.com>
2023-06-22 16:02:23 +0700

    make upload logic

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,24 +5,36 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+
 
 class UserController extends Controller
 {
-    public function store(Request $request, $id){
+    public function store(Request $request){
        $request->validate([
         'name' => 'required',
         'email' => 'required|email|unique:users',
         'token' => 'required',
         'image' => 'required|image',
        ]);
-       $user = User::findOrFail($id);
+    }
+    public function uploadPhoto(Request $request){
+        $request->validate([
+            'image'=>'required',
+        ]);
+        $user = Auth::user();
 
-       $imagePath =$request->file('image')->store('public/images');
-       $relativeImagePath = str_replace('public', 'storage', $imagePath);
+        if($user->image){
+            Storage::delete('assets/images/'.$user->image);
+        }
+        $filename = $request->file('image')->getClientOriginalName();
+        $hash = Str::random(10). '.' . $filename;
+        $filepath = $request->file('image')->storeAs('assets/images', $hash, 'public');
 
-       $user->image = $relativeImagePath;
-       $user->save();
+        $user->image = $filepath;
+        $user->save();
 
-       return response()->json(['message' => 'Gambar berhasil disimpan']);
+        return redirect("/onboard");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\VerifikasiController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Auth;
@@ -38,5 +39,7 @@ Route::middleware('auth:sanctum', 'token')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::post('/users/{$id}/image', [UserController::class, 'store'])->name('upload.image');
 
 require __DIR__ . '/auth.php';
