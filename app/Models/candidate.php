dee3d6616a22e3fe575054b36573083f1750250c
AradheaRangga <aradhearangga36973@gmail.com>
2023-06-24 14:28:10 +0700

    relation migration

--- app/Models/candidate.php
+++ app/Models/candidate.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class candidate extends Model
+class Candidate extends Model
 {
     use HasFactory;
     protected $fillable = [
@@ -13,4 +13,12 @@ class candidate extends Model
         'candidate_name',
         'vote_count',
     ];
+
+    public function vote_bem(){
+        return $this->hasMany(Vote::class, 'bem_id', 'id');
+    }
+
+    public function vote_blm(){
+        return $this->hasMany(Vote::class, 'blm_id', 'id');
+    }
 }
