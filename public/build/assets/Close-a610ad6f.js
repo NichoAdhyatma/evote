d86ba8b73f99033f697f1384d9933c4d7bf99843
Nicholaus Adhyatma <nicholausadhyatma@gmail.com>
2023-06-25 12:10:21 +0700

    build-testing

---
+++ public/build/assets/Close-a610ad6f.js
@@ -0,0 +1,1 @@
+import{a as o}from"./app-e6603889.js";import{c as a}from"./createSvgIcon-3db1691b.js";const t=a(o("path",{d:"M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"}),"Close");export{t as C};
